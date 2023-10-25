0515b6c41df71284fb27f9ca30727fe9a269d4d6
Pedro Santana <pedro.ferraz01@gmail.com>
2023-10-25 15:58:50 -0300

    feat: skills at main page

---
+++ my-portfolio/src/components/SkillCard.tsx
@@ -0,0 +1,33 @@
+
+import Image from 'next/image'
+import React from 'react'
+import Button from './Button'
+
+type Props = {
+  data: Array<{
+    title: string,
+    content: string
+  }>
+}
+
+const SkillCard = ({data}: Props) => {
+  return (
+    <div className='grid grid-rows-2 grid-flow-col gap-4 mx-8 w-[600px]'>
+      {data.map((item,idx)=>{
+        return(
+          <div key={idx} className='border border-gray-400 flex-col justify-start items-start inline-flex w-[200px]'>       
+              <div className='border-b border-gray-400 p-2 w-full'>
+                <p className='font-medium text-white '>{item.title}</p>
+              </div>
+              <div className='p-2'>
+                <p className='text-[#ABB2BF]'>{item.content}</p>
+              </div>
+          </div>
+        )
+      })}
+      <div></div>
+    </div>
+  )
+}
+
+export default SkillCard
--- my-portfolio/src/components/Skills.tsx
+++ my-portfolio/src/components/Skills.tsx
@@ -1,10 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import SkillCard from './SkillCard'
 
 type Props = {}
 
+const skills = [{title : 'Linguagens', content: 'Javascript Typescript Python C#'}, {title:'Bancos de Dados', content:'MySQL Postgres SqlServer'}, {title:'Ferramentas',content:'VSCode Visual Studio Linux Docker Jenkins Git Figma Github Actions,AWS'}, {title:'Frameworks',content: 'React Express.js NestJs Next.js  '}, {title:'Outros',content:' HTML CSS TAILWINDCSS SCSS BOOTSTRAP'}]
 const Skills = (props: Props) => {
   return (
-    <div>Skills</div>
+    <div className='w-[1500px] mx-auto mt-36'>
+      <div className='mb-14 flex justify-between'>
+        <div className='flex gap-4'>
+          <h1 className='text-white font-medium text-3xl'><span className='text-[#C778DD]'>#</span>skills</h1>
+          <Image src={'/line2.svg'} alt='line2' width={800} height={2}/>
+        </div>
+      </div>
+      <SkillCard data={skills}/>
+    </div>
   )
 }
 
