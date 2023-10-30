9adaa831afab87cb40b151884e3fb7cf10fa4448
Pedro Santana <pedro.ferraz01@gmail.com>
2023-10-30 14:37:24 -0300

    refactor: minor words change

--- my-portfolio/src/components/Button.tsx
+++ my-portfolio/src/components/Button.tsx
@@ -12,7 +12,7 @@ type Props = {
 
 const Button = ({content,type,redirect}: Props) => {
   return (
-    <Link href={redirect} className={type === 'red' ? 'text-center w-[110px] 2xl:w-[150px] h-[40px] border border-[#C778DD] px-2 2xl:px-4 py-2 text-xs 2xl:text-base text-white font-medium': 'text-center w-[110px] 2xl:w-[150px] h-[40px] border border-[#ABB2BF] px-3 2xl:px-4 py-2 text-xs 2xl:text-base text-white font-medium'}> {content} </Link>
+    <Link href={redirect} className={type === 'red' ? 'text-center w-[110px] 2xl:w-[200px] h-[40px] border border-[#C778DD] px-2 2xl:px-4 py-2 text-xs 2xl:text-base text-white font-medium': 'text-center w-[110px] 2xl:w-[200px] h-[40px] border border-[#ABB2BF] px-3 2xl:px-4 py-2 text-xs 2xl:text-base text-white font-medium'}> {content} </Link>
   )
 }
 
--- my-portfolio/src/components/Intro.tsx
+++ my-portfolio/src/components/Intro.tsx
@@ -9,8 +9,8 @@ function Intro({}: Props) {
     <div className='mt-12 lg:mt-32 w-[88%] 2xl:w-[1500px] 2xl:h-[423px] mx-auto flex flex-wrap justify-center lg:justify-between items-center'>
       <div className='flex flex-col gap-8 w-[300px] 2xl:w-[800px] 2xl:h-[227px] py-20'>
         <h1 className='text-2xl lg:text-3xl xl:text-4xl font-semibold text-white'>Pedro é um <span className='text-[#C778DD]'>Desenvolvedor FullStack</span> e <span className='text-[#C778DD]'>Devops/Cibersegurança Entusiasta</span></h1>
-        <p className='text-base text-[#ABB2BF] font-normal'>Ele faz aplicações responsiveis e funcionais além de perfomáticas</p>
-        <Button content='Contate-me!!' type='red' redirect='/contatos'/> 
+        <p className='text-base text-[#ABB2BF] font-normal'>Faço aplicações responsiveis e funcionais além de perfomáticas</p>
+        <Button content='Contate-me!' type='red' redirect='/contatos'/> 
       </div>
       <div >
         <Image src={'/foto1.png'} alt='foto1' width={457} height={386} className='border-b border-r border-[#C778DD] '/>
