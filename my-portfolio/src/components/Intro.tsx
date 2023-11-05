a8f264be4f95d82cf9df790766ad6f5e71008011
pedro santana <pedro.ferraz01@gmail.com>
2023-11-04 22:23:28 -0300

    style: change margin

--- my-portfolio/src/components/Intro.tsx
+++ my-portfolio/src/components/Intro.tsx
@@ -6,8 +6,8 @@ type Props = {}
 
 function Intro({}: Props) {
   return (
-    <div className='mt-12 lg:mt-32 w-[88%] 2xl:w-[1500px] 2xl:h-[423px] mx-auto flex flex-wrap justify-center lg:justify-between items-center'>
-      <div className='flex flex-col gap-8 w-[300px] 2xl:w-[800px] 2xl:h-[227px] py-20'>
+    <div className='mt-12 lg:mt-24 w-[88%] 2xl:ml-20 2xl:w-[1500px] 2xl:h-[423px] mx-auto flex flex-wrap justify-center lg:justify-between items-center'>
+      <div className='flex flex-col gap-8 w-[300px] 2xl:w-[800px] 2xl:h-[227px] py-8'>
         <h1 className='text-2xl lg:text-3xl xl:text-4xl font-semibold text-white'>Pedro é um <span className='text-[#C778DD]'>Desenvolvedor FullStack</span> e <span className='text-[#C778DD]'>Devops/Cibersegurança Entusiasta</span></h1>
         <p className='text-base text-[#ABB2BF] font-normal'>Faço aplicações responsivas e funcionais além de performáticas</p>
         <Button content='Contate-me!' type='red' redirect='/contatos'/> 
