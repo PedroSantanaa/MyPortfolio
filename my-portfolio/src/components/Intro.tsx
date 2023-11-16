180d79cf7bfc9ec2e7b899b87cf27a23f2c297fa
Pedro Santana <pedro.ferraz01@gmail.com>
2023-11-16 18:59:37 -0300

    style: change photo and add some styles

--- my-portfolio/src/components/Intro.tsx
+++ my-portfolio/src/components/Intro.tsx
@@ -6,14 +6,14 @@ type Props = {}
 
 function Intro({}: Props) {
   return (
-    <div id='home' className='mt-12 lg:mt-32 w-[88%] 2xl:w-[1500px] 2xl:h-[423px] mx-auto flex flex-wrap justify-center lg:justify-between items-center'>
-      <div className='flex flex-col gap-8 w-[300px] 2xl:w-[800px] 2xl:h-[227px] py-20'>
+    <div id='home' className='mt-28 lg:mt-44 w-[88%] 2xl:w-[1500px] 2xl:h-[423px] mx-auto flex flex-wrap justify-center sm:justify-between items-center'>
+      <div className='flex flex-col gap-8 w-[300px] 2xl:w-[800px] 2xl:h-[227px]'>
         <h1 className='text-2xl lg:text-3xl xl:text-4xl font-semibold text-white'>Pedro é um <span className='text-[#C778DD]'>Desenvolvedor FullStack</span> e <span className='text-[#C778DD]'>Devops/Cibersegurança Entusiasta</span></h1>
         <p className='text-base text-[#ABB2BF] font-normal'>Faço aplicações responsivas e funcionais além de performáticas</p>
-        <Button content='Contate-me!' type='red' redirect='/contatos'/> 
+        <Button content='Contate-me!' type='red' redirect='#contato'/> 
       </div>
-      <div >
-        <Image src={'/foto1.png'} alt='foto1' width={457} height={386} className='border-b border-r border-[#C778DD] '/>
+      <div>
+        <Image src={'/mainFoto.svg'} alt='foto1' width={540} height={478} className='hidden w-[350px] h-[402px] sm:block lg:w-full lg:h-full'/>
       </div>
     </div>
   )
