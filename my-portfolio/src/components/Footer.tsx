70e7043fcd3d3caa4eb20d89853ce9dd7ab71dd4
Pedro Santana <pedro.ferraz01@gmail.com>
2023-11-17 11:02:54 -0300

    feat: add icons and button

--- my-portfolio/src/components/Footer.tsx
+++ my-portfolio/src/components/Footer.tsx
@@ -1,34 +1,33 @@
 import Image from 'next/image'
 import React from 'react'
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import EmailIcon from '@mui/icons-material/Email';
 import Link from 'next/link';
 
 type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <div className='border-t border-gray-400 py-8 flex flex-col gap-10 justify-center items-center mt-36'>
-      <div className='flex flex-wrap gap-4 justify-center items-center 2xl:justify-between w-[88%] 2xl:w-[1200px]'>
-        <div className='2xl:pb-12'>
-          <div className='flex gap-2 pb-4 justify-center items-center lg:justify-normal'>
-            <Image src={"/letraP.png"} alt='letraP' width={10} height={10}/>
-            <p className='font-bold text-white text-xs sm:text-base'>Pedro</p>
-            <p className='text-gray-400 text-xs sm:text-base'>pedro.ferraz01@gmail.com</p>
+    <div className='bg-black'>
+      <div className='border-t border-white flex flex-col justify-center items-center '>
+        <div className='flex flex-wrap gap-4 justify-center items-center 2xl:justify-between w-[88%] 2xl:w-[1500px]'>
+          <div className='2xl:pb-12'>
+            <div className='flex gap-2 pb-4 justify-center items-center lg:justify-normal'>
+              <Link href={"#top"}><Image src={"/logoPedro.svg"} alt='logoP' width={206} height={92}></Image></Link>
+            </div>
+            <p className='text-gray-400'>Desenvolvedor Fullstack e Devops/Cibersegurança Entusiasta</p>
+            <div className='flex gap-2'>
+              <Link href={'https://github.com/PedroSantanaa'} target='_blank'><Image src={'/githubIcon.svg'} width={40} height={40} alt='githubIcon'/></Link>
+              <Link href={'https://www.linkedin.com/in/pedro-santana-7305151a5/'} target='_blank'><Image src={'/linkedinIcon.svg'} width={40} height={40} alt='linkedinIcon'/></Link>
+              <Link href={'mailto:pedro.ferraz01@gmail.com'} target='_blank'><Image src={'/emailIcon.svg'} width={40} height={40} alt='emailIcon'/></Link>
+            </div>
           </div>
-          <p className='text-gray-400'>Desenvolvedor Fullstack e Devops/Cibersegurança Entusiasta</p>
-        </div>
-        <div className='flex flex-col gap-3'>
-          <p className='text-white font-bold text-2xl'>Mídia</p>
-          <div className='flex gap-2 '>
-            <Link href={'https://github.com/PedroSantanaa'} target='_blank'><GitHubIcon className='text-gray-400'/></Link>
-            <Link href={'https://www.linkedin.com/in/pedro-santana-7305151a5/'} target='_blank'><LinkedInIcon className='text-gray-400'/></Link>
-            <Link href={'mailto:pedro.ferraz01@gmail.com'} target='_blank'><EmailIcon className='text-gray-400'/></Link>
+          <div className='flex flex-col gap-3'>
+            <div className='flex gap-2'>
+              <Link href={"#contato"} className='border border-white rounded-lg px-7 py-2 text-white text-xl'><span className='text-[#C778DD]'>#</span><span >contate-me</span></Link>         
+            </div>
           </div>
         </div>
+          <p className='text-white text-xl font-inika font-bold'>© Copyright 2023. Feito por Pedro</p>
       </div>
-        <p className='text-gray-400'>© Copyright 2023. Feito por Pedro</p>
     </div>
 
   )
