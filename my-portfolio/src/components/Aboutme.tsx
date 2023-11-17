a3ebad4fb0de5ead21a754376fa9eb14b7d942eb
Pedro Santana <pedro.ferraz01@gmail.com>
2023-11-17 11:02:22 -0300

    feat: add exp and new responsive

--- my-portfolio/src/components/Aboutme.tsx
+++ my-portfolio/src/components/Aboutme.tsx
@@ -10,17 +10,17 @@ type Props={
 
 const Aboutme = ({titleIcon}:Props) => {
   return (
-    <div className='h-[720px]'>
+    <div className='h-[1200px] md:h-[800px] lg:h-[720px]'>
       <div id='sobre' className='w-[88%] 2xl:w-[1500px] 2xl:h-[508px] mx-auto pt-10'>
-        <div className='mb-14 flex flex-col gap-6'>
+        <div className='mb-5 flex flex-col gap-6'>
           <div className='flex gap-4'>
             <Image src={'/line4.svg'} alt='line2' width={95} height={2} />
             <h1 className='text-black font-medium text-3xl'><span className='text-[#C778DD]'>{titleIcon}</span>sobre mim</h1>
-            <Image src={'/line2.svg'} alt='line2' width={167} height={2} />
+            <Image src={'/line2.svg'} alt='line2' width={167} height={2} className='hidden md:block' />
           </div>
         </div>
-        <div className='flex gap-10 text-center'>
-          <div className='flex flex-col gap-7 w-[650px]'>
+        <div className='flex flex-col lg:flex-row gap-10 text-center'>
+          <div className='flex flex-col gap-7 w-[80%] md:w-[650px] text-left'>
               <p className='text-black'>Olá, meu nome é Pedro Santana Ferraz de Oliveira!!</p>
               <p className='text-black text-xs 2xl:text-base'>Eu sou um desenvolvedor fullstack auto didata, moro na Bahia,
               Brasil. Eu posso desenvolver aplicações responsivas e funcionais desde o rascunho até o projeto final e de forma objetiva e performática, tanto aplicações front-end quanto back-end.</p>
