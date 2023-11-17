2be78ad057235843b2276612f2443ac59735e526
Pedro Santana <pedro.ferraz01@gmail.com>
2023-11-17 11:03:07 -0300

    style: style change

--- my-portfolio/src/components/Skills.tsx
+++ my-portfolio/src/components/Skills.tsx
@@ -11,10 +11,10 @@ const Skills = (props: Props) => {
     <div className='bg-black h-[720px]'>
       <div id='skills' className='w-[88%] 2xl:w-[1500px] mx-auto pt-10'>
         <div className='flex flex-wrap justify-between'>
-          <div className='flex gap-4'>
+          <div className='flex gap-4 mb-5'>
               <Image src={'/line4.svg'} alt='line2' width={95} height={2} />
               <h1 className='text-white font-medium text-3xl'><span className='text-[#C778DD]'>#</span>skills</h1>
-              <Image src={'/line2.svg'} alt='line2' width={167} height={2} />
+              <Image src={'/line2.svg'} alt='line2' width={167} height={2} className='hidden md:block' />
           </div>
         </div>
         <SkillCard data={skills}/>
